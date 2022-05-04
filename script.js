6453ac38fe3cebad48f8a00746d7814b9656d83f
Glitch (glitch-hello-website) <none>
2022-05-04 18:53:00 +0000

    ⚾️🌕 Checkpoint ./script.js:44485223/183 ./.glitch-assets:44485223/2574 ./README.md:44485223/3402

--- script.js
+++ script.js
@@ -7,4 +7,10 @@ new Phaser.Game({
       this.add.text(0, 0, "Hello 🌎", { font: '72px sans-serif' });
     }
   }
-})
+});
+
+/*
+this.load.image('sky', 'assets/skies/space3.png');
+this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+this.load.image('red', 'assets/particles/red.png');
+*/
